da3e7ffa094f2aa83a8ccc5d7ab223e5decf7dcc
Pavlo_Sernetskyi <74708976+PavloSernetskyi@users.noreply.github.com>
2025-05-20 16:38:51 -0700

    eliminated the params.slug async warning, cleaned up codebase

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import { db } from '../../../db/drizzle';
-import { posts } from '../../../db/schema';
+import { db } from '@/db/drizzle';
+import { posts } from '@/db/schema';
 import { eq } from 'drizzle-orm';
 import { notFound } from 'next/navigation';
 import Navbar from '@/components/Navbar';
@@ -8,21 +8,21 @@ interface PostPageProps {
   params: { slug: string };
 }
 
-export default async function PostPage({ params }: PostPageProps) {
+
+export default async function PostPage(props: PostPageProps) {
+  const { slug } = await props.params;
+
   const post = await db
     .select()
     .from(posts)
-    .where(eq(posts.slug, params.slug))
+    .where(eq(posts.slug, slug))
     .then((res) => res[0]);
 
   if (!post) return notFound();
 
   return (
     <div className="bg-white text-black min-h-screen">
-        {/* Reusable Navbar */}
       <Navbar />
-
-      {/* Post Content */}
       <main className="max-w-3xl mx-auto p-6">
         <h1 className="text-4xl font-bold mb-2">{post.title}</h1>
         <p className="text-gray-600 mb-8">by {post.authorName}</p>
